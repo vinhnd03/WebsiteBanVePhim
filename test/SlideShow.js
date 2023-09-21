0285b203c86a90d7fc9bde682358bbc14beb61b7
Lê Hữu Tưởng <lehuutuong.312@gmail.com>
2023-09-21 10:02:20 +0700

    Create SlideShow.js

---
+++ test/SlideShow.js
@@ -0,0 +1,15 @@
+// table Javascript để điều khiển Slide Show
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Đổi hình sau mỗi 2 giây
+}
