3439ba4eb22c8198b26260448d1a08bdc3458bdf
Lê Hữu Tưởng <lehuutuong.312@gmail.com>
2023-09-28 09:02:26 +0700

    Create DatnApplicationTests.java

---
+++ TicketMen/datn/src/test/java/nhom3/datn/DatnApplicationTests.java
@@ -0,0 +1,13 @@
+package nhom3.datn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
