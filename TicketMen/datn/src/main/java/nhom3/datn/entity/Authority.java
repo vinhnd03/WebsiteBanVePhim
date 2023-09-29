0ece789e4894a59e7e5ec137746b6c2891355e0f
vinhnd03 <ducvinhmycanh@gmail.com>
2023-09-29 09:51:02 +0700

    Create Authority.java

---
+++ TicketMen/datn/src/main/java/nhom3/datn/entity/Authority.java
@@ -0,0 +1,31 @@
+package nhom3.datn.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@SuppressWarnings("*")
+@Data
+@Entity
+@Table(name = "Authorities")
+public class Authority implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "Username")
+    Account account;
+    
+    @ManyToOne
+    @JoinColumn(name = "RoleId")
+    Role role;
+}
