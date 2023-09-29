e6697cac5a0c5b0db7ecc89d8278633556c0197a
vinhnd03 <ducvinhmycanh@gmail.com>
2023-09-29 09:51:15 +0700

    Create Room.java

---
+++ TicketMen/datn/src/main/java/nhom3/datn/entity/Room.java
@@ -0,0 +1,28 @@
+package nhom3.datn.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@SuppressWarnings("*")
+@Data
+@Entity
+@Table(name = "Rooms")
+public class Room implements Serializable {
+    @Id
+    String roomId;
+    String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "room")
+    List<Ticket> tickets;
+}
