c3edc22ae497963d7d0f460fa3928a7ceccd2b1f
vinhnd03 <ducvinhmycanh@gmail.com>
2023-09-29 10:28:46 +0700

    Create AuthorityDao.java

---
+++ TicketMen/datn/src/main/java/nhom3/datn/dao/AuthorityDao.java
@@ -0,0 +1,9 @@
+package nhom3.datn.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nhom3.datn.entity.Authority;
+
+public interface AuthorityDao extends JpaRepository<Authority, Integer>{
+    
+}
