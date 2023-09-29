a0cccfe041c916e218138e2cbe42b80713fd7f7b
vinhnd03 <ducvinhmycanh@gmail.com>
2023-09-29 09:51:12 +0700

    Create Role.java

---
+++ TicketMen/datn/src/main/java/nhom3/datn/entity/Role.java
@@ -0,0 +1,27 @@
+package nhom3.datn.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@SuppressWarnings("*")
+@Data
+@Entity
+@Table(name = "Roles")
+public class Role implements Serializable{
+    @Id
+    String id;
+    String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "role")
+    List<Authority> authorities;
+}
