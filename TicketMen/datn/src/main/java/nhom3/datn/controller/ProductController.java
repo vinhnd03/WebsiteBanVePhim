4a559dbbad4a0b796c1792b2c4d63cd8ac1d63ff
vinhnd03 <ducvinhmycanh@gmail.com>
2023-10-02 16:05:03 +0700

    Update ProductController.java

--- TicketMen/datn/src/main/java/nhom3/datn/controller/ProductController.java
+++ TicketMen/datn/src/main/java/nhom3/datn/controller/ProductController.java
@@ -7,14 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
-<<<<<<< HEAD
         // @RequestMapping({"/index"})
     // public String index(){
     //     return "/admin/index.html";
     // }
 
     //hello hòn tửn 
-=======
-
->>>>>>> 50495714985557b37b80a5a3ae2a8f32abfe9cf8
 }
