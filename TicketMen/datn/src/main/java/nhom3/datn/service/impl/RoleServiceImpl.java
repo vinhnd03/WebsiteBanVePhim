b68ce6568c29f432d20b457a9bd7df8c4518cc08
hoangphan76 <141141651+hoangphan76@users.noreply.github.com>
2023-10-02 15:06:54 +0700

    Create RoleServiceImpl.java

---
+++ TicketMen/datn/src/main/java/nhom3/datn/service/impl/RoleServiceImpl.java
@@ -0,0 +1,21 @@
+package nhom3.datn.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import nhom3.datn.dao.RoleDao;
+import nhom3.datn.entity.Role;
+import nhom3.datn.service.RoleService;
+
+@Service
+public class RoleServiceImpl implements RoleService{
+    @Autowired
+    RoleDao dao;
+
+    @Override
+    public Role findById(String id) {
+        return dao.findById(id).get();
+    }
+
+   
+}
