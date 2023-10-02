8f024cacbe9a4ada6088c5e47aa59b8bfdd7c3b6
hoangphan76 <141141651+hoangphan76@users.noreply.github.com>
2023-10-02 16:23:19 +0700

    haongdeptraivl

--- TicketMen/datn/src/main/java/nhom3/datn/controller/ProductController.java
+++ TicketMen/datn/src/main/java/nhom3/datn/controller/ProductController.java
@@ -7,13 +7,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
-<<<<<<< HEAD
-=======
-        // @RequestMapping({"/index"})
-    // public String index(){
-    //     return "/admin/index.html";
-    // }
-
-    //hello hòn tửn 
->>>>>>> 4a559dbbad4a0b796c1792b2c4d63cd8ac1d63ff
+    
 }
