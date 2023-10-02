1efbeadf7799655ca9ffac4352e5389e1f2027aa
hoangphan76 <141141651+hoangphan76@users.noreply.github.com>
2023-10-02 09:29:01 +0700

    admin

---
+++ TicketMen/datn/src/main/java/nhom3/datn/controller/ProductController.java
@@ -0,0 +1,14 @@
+
+package nhom3.datn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+        @RequestMapping({"/index"})
+    public String home(){
+        return "layout/index";
+    }
+}
