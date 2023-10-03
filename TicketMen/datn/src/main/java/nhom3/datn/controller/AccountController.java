7eacc4534c0288066c828a01c8cc181db1743dbc
vinhnd03 <ducvinhmycanh@gmail.com>
2023-10-03 10:40:40 +0700

    sửa đăng ký

--- TicketMen/datn/src/main/java/nhom3/datn/controller/AccountController.java
+++ TicketMen/datn/src/main/java/nhom3/datn/controller/AccountController.java
@@ -2,6 +2,7 @@ package nhom3.datn.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import nhom3.datn.entity.Account;
 import nhom3.datn.entity.Authority;
@@ -30,18 +32,16 @@ public class AccountController {
     AuthorityService authorityService;
 
     @PostMapping("account/add")
-    public String add(Model model){
-        Account account = new Account();; 
+    public String add(Model model, @ModelAttribute("account") Account account,
+            @RequestParam("gender") String gender){
         Authority authority = new Authority();
         Role role = roleService.findById("USER");
 
         authority.setAccount(account);
         authority.setRole(role);
-        
 
-
-        account.setUsername("hello2");
-        
+        System.out.println(gender);
+        // account.setGender(gender);
         accountService.save(account);
         authorityService.save(authority);
         return "redirect:/home/index";
