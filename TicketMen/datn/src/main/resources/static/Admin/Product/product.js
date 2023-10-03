b62bb8391b619eda09a71a4621cddb92e9d7cfb7
Lê Hữu Tưởng <lehuutuong.312@gmail.com>
2023-10-03 09:23:10 +0700

    tuong sua

--- TicketMen/datn/src/main/resources/static/Admin/Product/product.js
+++ TicketMen/datn/src/main/resources/static/Admin/Product/product.js
@@ -1,4 +1,4 @@
-app.controller("product-ctrl", function($scope, $http){
+app.controller("product", function($scope, $http){
     $scope.items = [];
     $scope.cates = [];
     $scope.form = {};
@@ -20,7 +20,7 @@ app.controller("product-ctrl", function($scope, $http){
 
     $scope.initialize = function(){
         //load products
-        $http.get("/rest/products").then(resp => {
+        $http.get("/Admin/products").then(resp => {
             $scope.items = resp.data;
             $scope.items.forEach(item => {
                 item.createDate = new Date(item.createDate)
