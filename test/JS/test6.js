31848b790672225de1cc1a2f82bc2cc0c0b0219c
thanhlamvip1 <97095515+thanhlamvip1@users.noreply.github.com>
2023-11-04 15:52:35 +0700

    test 6 nha

--- test/JS/test6.js
+++ test/JS/test6.js
@@ -1,104 +1,81 @@
-// Lấy các phần tử HTML bạn cần thao tác
-const quantityInputs = document.querySelectorAll('.ticket-num input');
-const ticketPrices = Array.from(document.querySelectorAll('.ticket-price')).map(ticketPriceElement => parseInt(ticketPriceElement.getAttribute('data-price')));
-const ticketTotalElements = document.querySelectorAll('.ticket-total span');
-const totalTicketNumElement = document.querySelector('.total-ticket-num span');
-const totalTicketAmountElement = document.querySelector('.total-ticket-amount span');
-const orderWrapTotalAmountElement = document.querySelector('.order-wrap .about-ticket li:nth-child(4) .value span');
+const seatsContainer = document.querySelector(".seats");
+const rows = 12; // Số hàng
+const seatsPerRow = 10; // Số ghế trong mỗi hàng
+const seatAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const pricePerSeat = 50000; // Giá mỗi ghế là 50,000 VNĐ
 
-// Khởi tạo mảng lưu trữ số lượng và giá trị tổng
-let quantities = [0, 0];
+let selectedSeats = [];
+let availableSeats = rows * seatsPerRow;
 
-// Hàm cập nhật số lượng và giá trị tổng cho từng loại vé
-function updateQuantityAndTotal(index) {
-    const quantity = quantities[index];
-    const price = ticketPrices[index];
-    const totalAmount = quantity * price;
+const selectedSeatsList = document.getElementById('selected-seats');
+const totalAmountElement = document.getElementById('total-amount');
 
-    quantityInputs[index].value = quantity;
-    ticketTotalElements[index].textContent = totalAmount + 'đ';
-}
-
-// Hàm cập nhật tổng số vé và tổng số tiền
-function updateTotalTicket() {
-    const totalQuantity = quantities.reduce((acc, current) => acc + current, 0);
-    totalTicketNumElement.textContent = totalQuantity;
-    const totalAmount = quantities.reduce((acc, current, index) => acc + current * ticketPrices[index], 0);
-    totalTicketAmountElement.textContent = totalAmount + ',00';
-
-    // Cập nhật giá ở order-wrap
-    orderWrapTotalAmountElement.textContent = totalAmount + 'đ';
-}
-
-// Xử lý sự kiện khi tăng số lượng
-function handleIncrease(index) {
-    quantities[index]++;
-    updateQuantityAndTotal(index);
-    updateTotalTicket();
-}
-
-// Xử lý sự kiện khi giảm số lượng
-function handleDecrease(index) {
-    if (quantities[index] > 0) {
-        quantities[index]--;
-        updateQuantityAndTotal(index);
-        updateTotalTicket();
+// Tạo ghế và theo dõi trạng thái của ghế
+for (let i = 0; i < rows; i++) {
+    for (let j = 1; j <= seatsPerRow; j++) {
+        const seat = document.createElement("div");
+        seat.classList.add("seat");
+        seat.textContent = seatAlphabet[i] + j;
+        seatsContainer.appendChild(seat);
     }
 }
 
-// Gán sự kiện cho nút tăng và giảm trên cả hai loại vé
-document.querySelectorAll('.ticket-num .add').forEach((addButton, index) => {
-    addButton.addEventListener('click', () => {
-        handleIncrease(index);
-        // Kết hợp với mã JavaScript trước đó ở đây để cập nhật ticket-wrap
-        const ticketWrapQuantityInput = document.querySelector('.ticket-wrap .ticket-num input');
-        const orderWrapQuantityElement = document.querySelector('.order-wrap .about-ticket li:nth-child(3) .value span');
-        ticketWrapQuantityInput.value = quantities[index];
-        orderWrapQuantityElement.textContent = quantities[index];
+const updateSeatStatus = () => {
+    selectedSeatsList.textContent = selectedSeats.length;
 
-        // Cập nhật giá ở order-wrap
-        orderWrapTotalAmountElement.textContent = totalTicketAmountElement.textContent;
-    });
-});
+    let totalAmount = selectedSeats.length * pricePerSeat;
 
-document.querySelectorAll('.ticket-num .minus').forEach((minusButton, index) => {
-    minusButton.addEventListener('click', () => {
-        handleDecrease(index);
-        // Kết hợp với mã JavaScript trước đó ở đây để cập nhật ticket-wrap
-        const ticketWrapQuantityInput = document.querySelector('.ticket-wrap .ticket-num input');
-        const orderWrapQuantityElement = document.querySelector('.order-wrap .about-ticket li:nth-child(3) .value span');
-        ticketWrapQuantityInput.value = quantities[index];
-        orderWrapQuantityElement.textContent = quantities[index];
+    totalAmountElement.textContent = totalAmount;
+};
 
-        // Cập nhật giá ở order-wrap
-        orderWrapTotalAmountElement.textContent = totalTicketAmountElement.textContent;
-    });
-});
-
-// Xử lý mặc định khi tải trang
-updateTotalTicket();
-
-//đếm ngược 5 phút hi
-    function demNguoc() {
-        var demNguocElement = document.getElementById("demNguoc");
-        var thoiGian = 300;
-
-        var x = setInterval(function () {
-            var phut = Math.floor(thoiGian / 60);
-            var giay = thoiGian % 60;
-
-
-            var thoiGianChuoi = phut + ":" + (giay < 10 ? "0" : "") + giay;
-
-            demNguocElement.innerHTML = thoiGianChuoi;
-
-            thoiGian--;
-
-            if (thoiGian < 0) {
-                clearInterval(x);
-                demNguocElement.innerHTML = "Hết thời gian";
-            }
-        }, 1000);
+seatsContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("seat")) {
+        const seat = e.target;
+        if (seat.classList.contains("selected")) {
+            seat.classList.remove("selected");
+            seat.classList.add("available");
+            selectedSeats.pop();
+            availableSeats++;
+        } else {
+            seat.classList.add("selected");
+            seat.classList.remove("available");
+            selectedSeats.push(seat);
+            availableSeats--;
+        }
+        updateSeatStatus();
     }
+});
 
-    window.onload = demNguoc;
+// Xử lý sự kiện khi nút Quay lại được nhấn
+const goBackButton = document.getElementById('back-button');
+goBackButton.addEventListener('click', () => {
+    // Đặt lại trạng thái ghế đã chọn và số tiền
+    selectedSeats.forEach(seat => {
+        seat.classList.remove('selected');
+        seat.classList.add('available');
+    });
+    selectedSeats = [];
+    availableSeats = rows * seatsPerRow;
+    updateSeatStatus();
+});
+
+// Xử lý sự kiện khi nút Tiếp tục được nhấn
+const continueButton = document.getElementById('continue-button');
+continueButton.addEventListener('click', () => {
+    // Thêm mã xử lý khi người dùng muốn tiếp tục với việc đặt vé
+    // Ở đây, bạn có thể thêm mã JavaScript để chuyển người dùng đến trang đặt vé hoặc thực hiện các thao tác khác.
+    alert('Đã chuyển đến trang đặt vé.');
+
+    // Để thay đổi suất chiếu, bạn có thể sử dụng một số hàm hoặc đoạn mã tùy theo cấu trúc trang web của bạn. 
+    // Dưới đây là một ví dụ đơn giản về cách thay đổi suất chiếu:
+    const showtimeElement = document.querySelector('.showtime-button.selected');
+    if (showtimeElement) {
+        // Lấy giá trị suất chiếu đã chọn
+        const selectedShowtime = showtimeElement.textContent;
+
+        // Thực hiện xử lý tiếp theo với suất chiếu đã chọn
+        alert(`Đã chọn suất chiếu: ${selectedShowtime}`);
+    } else {
+        alert('Vui lòng chọn một suất chiếu trước khi tiếp tục.');
+    }
+});
