9397a8161776b67d46fbbe838dc69e9a7683e6f0
vinhnd03 <ducvinhmycanh@gmail.com>
2023-11-16 15:55:49 +0700

    update forgot passwword

--- TicketMen/datn/src/main/java/nhom3/datn/controller/SecurityController.java
+++ TicketMen/datn/src/main/java/nhom3/datn/controller/SecurityController.java
@@ -1,11 +1,28 @@
 package nhom3.datn.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import nhom3.datn.entity.Account;
+import nhom3.datn.service.AccountService;
+import nhom3.datn.service.EmailService;
 
 @Controller
 public class SecurityController {
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    EmailService emailService;
+
     @RequestMapping("/security/login/form")
     public String loginForm(Model model){
         model.addAttribute("message", "Vui lòng đăng nhập");
@@ -33,7 +50,29 @@ public class SecurityController {
     @RequestMapping("/security/logoff/success")
     public String logoutSuccess(Model model){
         model.addAttribute("message", "Bạn đã đăng xuất");
-        return "redirect:/";
+        return "forward:/";
     }
 
+    @RequestMapping("/security/forgot")
+    public String forgot(Model model){        
+        return "security/forgot";
+    }
+
+    @RequestMapping("/security/sendMail")
+    public String sendEmail(Model model, @RequestParam(name = "emailConfirm", required = false) String email){ 
+
+        if (StringUtils.hasText(email)) {
+            Optional<Account> account = accountService.findByEmail(email);
+            if(account.isPresent()){
+                emailService.sendEmail(email, account.get());
+                model.addAttribute("message", "Vui long xac nhan tai khoan trong email");
+                return "/security/forgot";
+            }else{
+                model.addAttribute("message", "Email chua duoc dang ky");
+                return "/security/forgot";
+            }
+            
+		} 
+        return "/security/forgot";       
+    }
 }
