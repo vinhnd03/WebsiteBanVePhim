375a092bd5634c14827e90108f2d7a182055daa0
vinhnd03 <ducvinhmycanh@gmail.com>
2023-12-19 23:33:18 +0700

    update send email

--- TicketMen/datn/src/main/java/nhom3/datn/controller/rest/SendEmailRestController.java
+++ TicketMen/datn/src/main/java/nhom3/datn/controller/rest/SendEmailRestController.java
@@ -1,5 +1,7 @@
 package nhom3.datn.controller.rest;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import nhom3.datn.entity.Order;
 import nhom3.datn.service.ResetPasswordService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @CrossOrigin("*")
 @RestController
@@ -18,9 +21,10 @@ public class SendEmailRestController {
     @Autowired
     ResetPasswordService authenService;
 
-    @GetMapping("sendOrder")
-    public void sendEmail(@RequestParam("Email") String param, @RequestParam("Order") Order order) {
-        
+    @PostMapping("sendOrderDetail")
+    public void sendEmail(@RequestBody Map<String, Object> requestData) {
+        Long orderId = Long.parseLong(requestData.get("orderId").toString());
+        String username = requestData.get("username").toString();
+        authenService.orderSuccess(username, orderId);
     }
-    
 }
